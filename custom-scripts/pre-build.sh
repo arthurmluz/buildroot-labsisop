0bf0d5e1b821fec76e7a2981c3f26d1701f779d4
Arthur Luz <81325710+arthurmluz@users.noreply.github.com>
2023-04-01 02:39:35 +0000

    T1

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+
+ip=$(ifconfig | awk '/inet/ {print $2}' | awk 'NR==2')
+
+cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/../custom-scripts/S41network-config-copy
+
+sed -i 's/<IP-DO-HOST>/$ip/g' $BASE_DIR/../custom-scripts/S41network-config-copy
+
+cp $BASE_DIR/../custom-scripts/S41network-config-copy $BASE_DIR/target/etc/init.d/S41network-config
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
